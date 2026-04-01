6c950f1adb1ddc189705dff2daf95898316e799d
Dilo1999 <nimeshdilshan440@gmail.com>
2026-04-01 14:35:39 +0530

    Add favicon and apple touch icon links to site layout for improved branding and user experience.

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'LITUS Group' }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon/LG-Favicon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('images/favicon/LG-Favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-h-screen">
